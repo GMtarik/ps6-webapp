130bc8a5de7da491e7743aaff538f46fb34535fe
GMtarik <lukejin@uw.edu>
2023-03-07 15:53:12 -0800

    done

---
+++ ps6/README.md
@@ -0,0 +1,8 @@
+# ps6-webapp
+#This code provides a Shiny app that displays monthly temperature readings from satellite observations of the lower troposphere from January 1979 to December 2020. The data was collected by the University of Alabama in Huntsville (UAH) and is provided by Remote Sensing Systems (RSS).
+
+#The app has three tabs:
+
+#Information: This page provides general information about the dataset.
+#Plot: This page allows the user to select a year and one or more months, and then displays a plot of the total temperature change by month for the selected year and months. The user can also select the point color and size. The text output below the plot displays the number of observations in the selected subset.
+#Table: This page allows the user to select a month, and then displays a table of the region and temperature for that month. The text output below the table displays the number of observations in the selected subset.
--- ps6/app.R
+++ ps6/app.R
@@ -1,51 +1,147 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+# Load required packages
 library(shiny)
+library(dplyr)
+library(ggplot2)
+library(plotly)
+library(DT)
+library(rsconnect)
+library(shiny)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
+# Load the dataset
+uah_data <- read.delim("UAH-lower-troposphere-long.csv.bz2")
+
+# Define UI
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  
+  # Set page title
+  titlePanel("UAH Lower Troposphere Temperature"),
+  
+  # Main panel with tabset
+  mainPanel(
+    tabsetPanel(
+      
+      # Opening page with dataset information
+      tabPanel("Information", 
+               tags$div(
+                 h2("Dataset Information"), 
+                 br(), 
+                 "This dataset contains monthly temperature readings from satellite observations 
+                 of the lower troposphere (the lowest layer of the Earth's atmosphere) from January 
+                 1979 to December 2020. The data was collected by the University of Alabama in 
+                 Huntsville (UAH) and is provided by Remote Sensing Systems (RSS).", 
+                 br(), 
+                 tags$div(
+                   tags$h2("General Information"),
+                   tags$p(
+                     tags$strong("Dataset: "), "UAH Lower Troposphere ",
+                     tags$em("Long-Term Data Record")
+                   ),
+                   tags$p(
+                     tags$strong("Source: "),
+                     "University of Alabama, Huntsville (UAH), ",
+                     tags$em("Global Temperature Monitoring")
+                   ),
+                   tags$p(
+                     tags$strong("Description: "),
+                     "This dataset provides monthly global average temperature anomalies in the Earth's lower atmosphere, measured from satellite microwave sensors."
+                   ),
+                   tags$p(
+                     tags$strong("Variables: "),
+                     "year, month, temperature anomaly (Kelvin)"
+                   )
+                 )
+               )),
+      
+      # Plot page with temperature graph
+      tabPanel("Plot", 
+               h1(strong("Check Temperature by Year")),
+               sidebarLayout(
+                 sidebarPanel(
+                   selectInput(
+                     inputId = "year", label = "Select Year:",
+                     choices = sort(unique(uah_data$year)), selected = NULL
+                   ),
+                   checkboxGroupInput(
+                     inputId = "mon", label = "Select Month(s):",
+                     choices = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"),
+                     selected = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
+                   ),
+                   selectInput(
+                     inputId = "color", label = "Select Point Color:",
+                     choices = c("blue", "red", "green"), selected = "blue"
+                   ),
+                   sliderInput(
+                     inputId = "size", label = "Select Point Size:",
+                     min = 1, max = 5, value = 3
+                   )
+                 ),
+                 mainPanel(
+                   plotlyOutput("chart1"),
+                   verbatimTextOutput("text1"),
+                   hr()
+                 )
+               )
+      ),
+      tabPanel("Table",
+               h1(strong("Define Region by Month")),
+               sidebarLayout(
+                 sidebarPanel(
+                   selectInput(
+                     inputId = "month", label = "Select Month:",
+                     choices = unique(uah_data$month)
+                   )
+                 ),
+                 mainPanel(
+                   dataTableOutput("chart2"),
+                   verbatimTextOutput("text2"),
+                   hr()
+                 )
+               )
+      )
     )
+  )
 )
 
-# Define server logic required to draw a histogram
+
+# Define server function
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  graph_data <- reactive({
+    uah_data %>%
+      filter(year == input$year) %>%
+      filter(month %in% input$mon) %>%
+      group_by(month) %>%
+      summarize(total_temp = sum(temp))
+  })
+  
+  output$chart1 <- renderPlotly({
+    ggplotly(
+      ggplot(graph_data(), aes(x = month, y = total_temp)) +
+        geom_point(color = input$color, size = input$size) +
+        labs(title = "Temperature change by month", x = "Month", y = "Temp Change") +
+        scale_x_continuous(breaks = 1:12)
+    )
+  })
+  
+  table_data <- reactive({
+    uah_data %>%
+      filter(month == input$month) %>%
+      select(region, temp)
+  })
+  
+  output$chart2 <- renderDataTable({
+    table_data()
+  })
+  
+  output$text1 <- renderText({
+    paste("Selected subset contains", nrow(graph_data()), "observations")
+  })
+  
+  output$text2 <- renderText({
+    paste("Selected subset contains", nrow(table_data()), "observations")
+  })
 }
 
-# Run the application 
+# Run the Shiny app
 shinyApp(ui = ui, server = server)
